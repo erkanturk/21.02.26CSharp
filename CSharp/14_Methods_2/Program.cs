3bd21ab34f231a8fe658269541e20b6b3040f63f
erkanturk <refres98@gmail.com>
2026-04-11 18:49:23 +0300

    deneme

---
+++ CSharp/14_Methods_2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14_Methods_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
